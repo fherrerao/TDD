15bee62ea1128daf1ddefb041cb504765c0f99d4
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-14 23:22:54 +0300

    Create fizbuzz method

--- solver.rb
+++ solver.rb
@@ -6,11 +6,20 @@ class Solver
     number * factorial(number - 1)
   end
 
-  def reverse string
+  def reverse(string)
     if string.is_a? String
-    string.reverse
-    else 
+      string.reverse
+    else
       raise ArgumentError
+    end
+  end
+
+  def fizzbuzz(number)
+    if (number % 3 == 0) && (number % 5 == 0) then 'fizzbuzz'
+    elsif number % 3 == 0 then 'fizz'
+    elsif number % 5 == 0 then 'buzz'
+    else
+      number.to_s
     end
   end
 end

094db97ee5b6094dd7c6330062b897a5cddff213
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-14 23:26:03 +0300

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,23 +1,21 @@
 class Solver
   def factorial(number)
-    raise ArgumentError if number < 0
-    return 1 if number == 0
+    raise ArgumentError if number.negative?
+    return 1 if number.zero?
 
     number * factorial(number - 1)
   end
 
   def reverse(string)
-    if string.is_a? String
-      string.reverse
-    else
-      raise ArgumentError
-    end
+    raise ArgumentError unless string.is_a? String
+
+    string.reverse
   end
 
   def fizzbuzz(number)
-    if (number % 3 == 0) && (number % 5 == 0) then 'fizzbuzz'
-    elsif number % 3 == 0 then 'fizz'
-    elsif number % 5 == 0 then 'buzz'
+    if (number % 3).zero? && (number % 5).zero? then 'fizzbuzz'
+    elsif (number % 3).zero? then 'fizz'
+    elsif (number % 5).zero? then 'buzz'
     else
       number.to_s
     end

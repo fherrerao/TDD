96a449b23c2ee8e57b265fe6fbef397e9f0f3630
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-14 23:22:25 +0300

    Create unit tests for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -28,4 +28,22 @@ describe Solver do
       expect(@solver.reverse('hello')).to eq('olleh')
     end
   end
+
+  context '#fizzbuzz' do
+    it 'return fizzbuzz if number is divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'return fizz if number is divisible by 3' do
+      expect(@solver.fizzbuzz(3)).to eq('fizz')
+    end
+
+    it 'return buzz if number is divisible by 5' do
+      expect(@solver.fizzbuzz(5)).to eq('buzz')
+    end
+
+    it 'return number if number is not divisible by 3 or 5' do
+      expect(@solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
